5b1d03fc331bb421f0dbda1ccf30048bfaba8196
chitra atmakuru & Evan Butler <student@epicodus.com>
2015-03-11 16:38:21 -0700

    pass third test

--- tests/FindReplaceTest.php
+++ tests/FindReplaceTest.php
@@ -32,5 +32,20 @@
             //Assert
             $this->assertEquals("hello universe universe", $result);
         }
+
+        function test_noWordToFind()
+        {
+            //Arrange
+            $test_letter = new FindReplace;
+            $user_string = "hello";
+            $find = "world";
+            $replace = "universe";
+
+            //Act
+            $result = $test_letter->makeFindReplace($user_string, $find, $replace);
+
+            //Assert
+            $this->assertEquals("Can't find word to replace.", $result);
+        }
     }
 ?>
